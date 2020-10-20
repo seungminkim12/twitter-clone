0ea08ef9358ce925a7d0e1e9615949fdf9b3793d
SeungminKim <ksm0057@naver.com>
2020-10-20 19:58:51 +0900

    reduce

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,20 @@ import { authService } from "fbase"
 import { auth } from 'firebase';
 
 function App() {
-  const [init, setInit] = useState(false)
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [init, setInit] = useState(false);
   const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
         setUserObj(user);
-      } else {
-        setIsLoggedIn(false)
-      }
+      } 
       setInit(true)
     })
   }, []);
   return (
     <div>
       <>
-        {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing..."}
+        {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} /> : "Initializing..."}
         <footer>&copy;{new Date().getFullYear()} Twitter-clone</footer>
       </>
     </div>
