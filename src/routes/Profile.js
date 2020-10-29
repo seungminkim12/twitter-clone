f9eaef6ffe8dc1b1313f59831a120829ad248e8c
SeungminKim <ksm0057@naver.com>
2020-10-29 21:46:43 +0900

    get my tweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,12 +1,26 @@
-import { authService } from "fbase";
-import React from "react";
+import { authService, dbService } from "fbase";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-export default () => {
+export default ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+
+  const getMyTweets = async () => {
+    const tweets = await dbService
+      .collection("tweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+    console.log(tweets.docs.map((doc) => doc.data()));
+  };
+
+  useEffect(() => {
+    getMyTweets();
+  }, []);
+
   return <button onClick={onLogOutClick}>Log out</button>;
 };
