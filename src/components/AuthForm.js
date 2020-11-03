7827f5000bf229605b93a5ceacc5fe09fdc5b9cf
SeungminKim <ksm0057@naver.com>
2020-11-03 23:01:26 +0900

    css #1

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -41,7 +41,7 @@ function AuthForm() {
 
   return (
     <>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmit} className="container">
         <input
           name="Email"
           type="text"
@@ -49,6 +49,7 @@ function AuthForm() {
           required
           value={email}
           onChange={onChange}
+          className="authInput"
         />
         <input
           name="Password"
@@ -57,11 +58,16 @@ function AuthForm() {
           required
           value={password}
           onChange={onChange}
+          className="authInput"
         />
-        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
-        {error}
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Log In"}
+          className="authInput  authSubmit"
+        />
+        {error && <span className="authError">{error}</span>}
       </form>
-      <span onClick={toggleAccount}>
+      <span onClick={toggleAccount} className="authSwitch">
         {newAccount ? "Log In" : "Create Account"}
       </span>
     </>
